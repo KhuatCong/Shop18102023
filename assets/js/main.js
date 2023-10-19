f21b909ee3fc557179800a304f3e30ca6a633208
KhuatCong <khuatphucong06092002@gmail.com>
2023-10-19 17:31:32 +0700

    hehehe

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById('menu-mobile').style.width="70%";
+    document.getElementById('overlay').style.width="30%";
+}
+
+function closeNav(){
+    document.getElementById('menu-mobile').style.width="0%";
+    document.getElementById('overlay').style.width="0%";
+}
